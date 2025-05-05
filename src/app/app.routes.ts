f01c95ab632a221707fcc13663f67ddeccd865d0
Donovan Pardo <dpardox@gmail.com>
2025-05-05 15:33:29 -0500

    Implement sign-in feature

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { SignInComponent } from './features/sign-in/sign-in.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'sign-in',
+    loadComponent: () => import('./features/sign-in/sign-in.component').then(m => m.SignInComponent)
+  },
+  { path: '', redirectTo: '/sign-in', pathMatch: 'full' },
+];
