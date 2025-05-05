2600fc53f3d88214250d9b0bc156cddec6fde902
Donovan Pardo <dpardox@gmail.com>
2025-05-05 17:16:58 -0500

    Implement reactive form for sign-in with email validation

--- src/app/features/sign-in/sign-in.component.ts
+++ src/app/features/sign-in/sign-in.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthLayout } from '@shared/layouts/auth/auth.layout';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-sign-in',
-  imports: [RouterModule, AuthLayout],
+  imports: [CommonModule, RouterModule, AuthLayout, ReactiveFormsModule],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.css'
 })
 export class SignInComponent {
+
+  private formBuilder = inject(FormBuilder);
+
+  public signInForm = this.formBuilder.group({
+    email: ['', [ Validators.required, Validators.email ]],
+  });
+
+  public signIn() {
+    if (this.signInForm.invalid) return;
+
+    const { email } = this.signInForm.value;
+
+    console.log({ email }); // TODO (dpardo): delete
+  }
 
 }
