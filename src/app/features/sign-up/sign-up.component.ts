3135e9be018667921ac32756a80a8c52856bcc9f
Donovan Pardo <dpardox@gmail.com>
2025-05-05 17:22:43 -0500

    Implement reactive form for sign-up with email validation

--- src/app/features/sign-up/sign-up.component.ts
+++ src/app/features/sign-up/sign-up.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AuthLayout } from '@shared/layouts/auth/auth.layout';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-sign-up',
-  imports: [RouterModule, AuthLayout],
+  imports: [CommonModule, RouterModule, AuthLayout, ReactiveFormsModule],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
 export class SignUpComponent {
+
+  private formBuilder = inject(FormBuilder);
+
+  public signUpForm = this.formBuilder.group({
+    email: ['', [ Validators.required, Validators.email ]],
+  });
+
+  public signUp() {
+    if (this.signUpForm.invalid) return;
+
+    const { email } = this.signUpForm.value;
+
+    console.log({ email }); // TODO (dpardo): delete
+  }
 
 }
